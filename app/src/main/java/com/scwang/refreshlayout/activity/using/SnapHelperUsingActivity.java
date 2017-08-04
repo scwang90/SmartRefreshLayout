98225d5675ce2b9f6dd4aab6f1253f746d57c248
wsc <scwang>
2017-08-04 20:14:21 +0800

    完成 1.0.3 版本

--- app/src/main/java/com/scwang/refreshlayout/activity/using/SnapHelperUsingActivity.java
+++ app/src/main/java/com/scwang/refreshlayout/activity/using/SnapHelperUsingActivity.java
@@ -1,14 +1,12 @@
 package com.scwang.refreshlayout.activity.using;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.LinearSnapHelper;
-import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SnapHelper;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -51,78 +49,20 @@ public class SnapHelperUsingActivity extends AppCompatActivity {
                     holder.image(R.id.imageView, model);
                 }
             });
-            LinearSnapHelper mMySnapHelper = new LinearSnapHelper();
+            SnapHelper mMySnapHelper = new PagerSnapHelper();
             mMySnapHelper.attachToRecyclerView(recyclerView);
         }
 
     }
 
     private Collection<Integer> loadModels() {
-        return Arrays.asList(R.mipmap.ic_item1, R.mipmap.ic_item2, R.mipmap.ic_item3, R.mipmap.ic_item1);
-    }
-
-
-    public class MySnapHelper extends LinearSnapHelper {
-
-        private OrientationHelper mHorizontalHelper;
-
-        @Nullable
-        @Override
-        public int[] calculateDistanceToFinalSnap(RecyclerView.LayoutManager layoutManager, View targetView) {
-            int[] out = new int[2];
-            if (layoutManager.canScrollHorizontally()) {
-                out[0] = distanceToStart(targetView, getHorizontalHelper(layoutManager));
-            } else {
-                out[0] = 0;
-            }
-
-            return out;
-        }
-
-        private int distanceToStart(View targetView, OrientationHelper helper) {
-            return helper.getDecoratedStart(targetView) - helper.getStartAfterPadding();
-        }
-
-        @Nullable
-        @Override
-        public View findSnapView(RecyclerView.LayoutManager layoutManager) {
-            return findStartView(layoutManager, getHorizontalHelper(layoutManager));
-        }
-
-        private View findStartView(RecyclerView.LayoutManager layoutManager,
-                                   OrientationHelper helper) {
-
-            if (layoutManager instanceof LinearLayoutManager) {
-                int firstChild = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
-                int lastChild = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
-                if (firstChild == RecyclerView.NO_POSITION) {
-                    return null;
-                }
-                if (lastChild == layoutManager.getItemCount() - 1) {
-                    return layoutManager.findViewByPosition(lastChild);
-                }
-
-                View child = layoutManager.findViewByPosition(firstChild);
-
-                if (helper.getDecoratedEnd(child) >= helper.getDecoratedMeasurement(child) / 2
-                        && helper.getDecoratedEnd(child) > 0) {
-                    return child;
-                } else {
-                    return layoutManager.findViewByPosition(firstChild + 1);
-                }
-            }
-
-            return super.findSnapView(layoutManager);
-        }
-
-
-        private OrientationHelper getHorizontalHelper(
-                @NonNull RecyclerView.LayoutManager layoutManager) {
-            if (mHorizontalHelper == null) {
-                mHorizontalHelper = OrientationHelper.createHorizontalHelper(layoutManager);
-            }
-            return mHorizontalHelper;
-        }
+        return Arrays.asList(
+                R.mipmap.image_weibo_home_1,
+                R.mipmap.image_weibo_home_2,
+                R.mipmap.image_weibo_home_1,
+                R.mipmap.image_weibo_home_2,
+                R.mipmap.image_weibo_home_1,
+                R.mipmap.image_weibo_home_2);
     }
 
 }
