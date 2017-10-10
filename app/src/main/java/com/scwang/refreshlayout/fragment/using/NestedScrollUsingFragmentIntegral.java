91073829c7cea11e15c0bcc4ea8b9328cada6f17
scwang90 <scwang90@hotmaiul.com>
2017-10-10 17:29:54 +0800

    添加demo 纯滚动模式示例 和 空页面 和 嵌套滚动

--- app/src/main/java/com/scwang/refreshlayout/fragment/using/NestedScrollUsingFragmentIntegral.java
+++ app/src/main/java/com/scwang/refreshlayout/fragment/using/NestedScrollUsingFragmentIntegral.java
@@ -52,7 +52,7 @@ public class NestedScrollUsingFragmentIntegral extends Fragment implements Adapt
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_using_nestedscroll, container, false);
+        return inflater.inflate(R.layout.fragment_using_nestedscroll_integral, container, false);
     }
 
     @Override
