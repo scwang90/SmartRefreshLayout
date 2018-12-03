b4a183c549b9cccd6e3f2be2b751f554cc780576
scwang90 <scwang90@hotmail.com>
2018-12-03 18:24:03 +0800

    给源码添加英文注释

--- method-count-layout.bat
+++ method-count-layout.bat
@@ -1,32 +1,37 @@
 @ECHO OFF
 
-@echo off
 set aa=伟大的中国！我为你自豪！
 echo 替换前：%aa%
 echo 替换后：%aa:中国=中华人民共和国%
 echo aa = %aa%
-set "aa=%aa:中国=中华人民共和国%"
+set aa=%aa:中国=中华人民共和国%
 echo aa = %aa%
-pause
-
 
 SET sdk=null
-FOR /F "eol=; tokens=2,2 delims==" %%i IN ('findstr /i "sdk.dir0" local.properties') DO SET sdk=%%i
+SET tool=null
+FOR /F "eol=; tokens=2,2 delims==" %%i IN ('findstr /i "sdk.dir" local.properties') DO SET sdk=%%i
+FOR /F %%i IN ('findstr /i "buildToolsVersion" %~dp0app\build.gradle') DO SET tool=%%i
+
+findstr /i "sdk.dir" local.properties
+findstr /i "buildToolsVersion" %~dp0app\build.gradle
+
+rem type %~dp0app\build.gradle
+rem type local.properties
+
+echo tool=%tool%
 
 IF %sdk% == null (
     ECHO 没找到SDK，脚本中止
     GOTO END
-) else (
-    ECHO SKD = %sdk%
 )
 
-
-
+SET sdk=%sdk:\\=\%
+SET sdk=%sdk:\:=:%
+set sdk=%sdk%\build-tools\28.0.3\dx.bat
 
 echo skd=%sdk%
-set sdk = sdk + "\build-tools\"
-echo skd=%sdk%
 
+rem %sdk%
 rem ./gradlew assemble
 rem dx --dex --verbose --no-strict --output=refresh-layout/build/intermediates/intermediate-jars/debug/classes.jar.dex refresh-layout/build/intermediates/intermediate-jars/debug/classes.jar
 rem https://github.com/mihaip/dex-method-counts
