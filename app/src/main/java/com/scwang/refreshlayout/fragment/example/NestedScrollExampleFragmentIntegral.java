71631fe56376651507a954a77f4ea803f1de254c
scwang90 <scwang90@hotmail.com>
2019-07-23 18:22:38 +0800

    1.验证 VIewPager 问题

--- app/src/main/java/com/scwang/refreshlayout/fragment/example/NestedScrollExampleFragmentIntegral.java
+++ app/src/main/java/com/scwang/refreshlayout/fragment/example/NestedScrollExampleFragmentIntegral.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -23,8 +26,11 @@ import com.scwang.refreshlayout.R;
 import com.scwang.refreshlayout.adapter.BaseRecyclerAdapter;
 import com.scwang.refreshlayout.adapter.SmartViewHolder;
 import com.scwang.refreshlayout.fragment.example.NestedScrollExampleFragment.Item;
+import com.scwang.smartrefresh.header.PhoenixHeader;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener;
 import com.youth.banner.Banner;
 import com.youth.banner.loader.ImageLoader;
 
@@ -34,7 +40,6 @@ import java.util.Collection;
 import java.util.List;
 
 import static android.R.layout.simple_list_item_2;
-import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 import static com.scwang.refreshlayout.R.mipmap.gif_header_repast;
 import static com.scwang.refreshlayout.R.mipmap.image_weibo_home_2;
 
@@ -42,9 +47,12 @@ import static com.scwang.refreshlayout.R.mipmap.image_weibo_home_2;
  * 使用示例-嵌套滚动-整体
  * A simple {@link Fragment} subclass.
  */
-public class NestedScrollExampleFragmentIntegral extends Fragment implements AdapterView.OnItemClickListener {
+public class NestedScrollExampleFragmentIntegral extends Fragment implements AdapterView.OnItemClickListener, OnRefreshLoadMoreListener {
 
-    private BaseRecyclerAdapter<Item> mAdapter;
+    private ViewPager mViewPager;
+    private SmartPagerAdapter mAdapter;
+
+//    private BaseRecyclerAdapter<Item> mAdapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -68,32 +76,35 @@ public class NestedScrollExampleFragmentIntegral extends Fragment implements Ada
         banner.setImages(Arrays.asList(image_weibo_home_2,gif_header_repast));
         banner.start();
 
-        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), VERTICAL));
-        recyclerView.setAdapter(mAdapter = new BaseRecyclerAdapter<Item>(buildItems(), simple_list_item_2, NestedScrollExampleFragmentIntegral.this) {
-            @Override
-            protected void onBindViewHolder(SmartViewHolder holder, Item model, int position) {
-                holder.text(android.R.id.text1, model.name());
-                holder.text(android.R.id.text2, model.name);
-                holder.textColorId(android.R.id.text2, R.color.colorTextAssistant);
-            }
-        });
+        mViewPager = root.findViewById(R.id.viewPager);
+        mViewPager.setAdapter(mAdapter = new SmartPagerAdapter(getChildFragmentManager()));
+//        RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+//        recyclerView.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), VERTICAL));
+//        recyclerView.setAdapter(mAdapter = new BaseRecyclerAdapter<Item>(buildItems(), simple_list_item_2, NestedScrollExampleFragmentIntegral.this) {
+//            @Override
+//            protected void onBindViewHolder(SmartViewHolder holder, Item model, int position) {
+//                holder.text(android.R.id.text1, model.name());
+//                holder.text(android.R.id.text2, model.name);
+//                holder.textColorId(android.R.id.text2, R.color.colorTextAssistant);
+//            }
+//        });
 
         RefreshLayout refreshLayout = root.findViewById(R.id.refreshLayout);
-        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
-            @Override
-            public void onLoadMore(@NonNull final RefreshLayout refreshLayout) {
-                refreshLayout.getLayout().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        mAdapter.loadMore(buildItems());
-                        refreshLayout.finishLoadMore();
-                    }
-                }, 2000);
-            }
-        });
+        refreshLayout.setOnRefreshLoadMoreListener(this);
+//        refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+//            @Override
+//            public void onLoadMore(@NonNull final RefreshLayout refreshLayout) {
+//                refreshLayout.getLayout().postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        mAdapter.loadMore(buildItems());
+//                        refreshLayout.finishLoadMore();
+//                    }
+//                }, 2000);
+//            }
+//        });
 
         TextView textView = root.findViewById(R.id.target);
         textView.setOnClickListener(new View.OnClickListener() {
@@ -104,12 +115,14 @@ public class NestedScrollExampleFragmentIntegral extends Fragment implements Ada
         });
     }
 
-    private Collection<Item> buildItems() {
-        List<Item> items = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            items.addAll(Arrays.asList(Item.values()));
-        }
-        return items;
+    @Override
+    public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+        mAdapter.fragments[mViewPager.getCurrentItem()].onRefresh(refreshLayout);
+    }
+
+    @Override
+    public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+        mAdapter.fragments[mViewPager.getCurrentItem()].onLoadMore(refreshLayout);
     }
 
     @Override
@@ -121,6 +134,91 @@ public class NestedScrollExampleFragmentIntegral extends Fragment implements Ada
         public void displayImage(Context context, Object path, ImageView imageView) {
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setImageResource((Integer)path);
+        }
+    }
+
+
+    public static class SmartPagerAdapter extends FragmentStatePagerAdapter {
+
+        private final SmartFragment[] fragments;
+
+        SmartPagerAdapter(FragmentManager fm) {
+            super(fm);
+            this.fragments = new SmartFragment[]{
+                    new SmartFragment(),new SmartFragment()
+            };
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments[position];
+        }
+    }
+
+    public static class SmartFragment extends Fragment {
+
+        private RecyclerView mRecyclerView;
+        private BaseRecyclerAdapter<Item> mAdapter;
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return mRecyclerView = new RecyclerView(inflater.getContext());
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
+            mRecyclerView.setAdapter(mAdapter = new BaseRecyclerAdapter<Item>(buildItems(), simple_list_item_2) {
+                @Override
+                protected void onBindViewHolder(SmartViewHolder holder, Item model, int position) {
+                    holder.text(android.R.id.text1, model.name());
+                    holder.text(android.R.id.text2, model.name);
+                    holder.textColorId(android.R.id.text2, R.color.colorTextAssistant);
+                }
+            });
+        }
+
+        private Collection<Item> buildItems() {
+            List<Item> items = new ArrayList<>();
+            for (int i = 0; i < 10; i++) {
+                items.addAll(Arrays.asList(Item.values()));
+            }
+            return items;
+        }
+
+
+        public void onRefresh(final RefreshLayout refreshLayout) {
+            refreshLayout.getLayout().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mAdapter.refresh(buildItems());
+                    refreshLayout.finishRefresh();
+                    refreshLayout.resetNoMoreData();//setNoMoreData(false);
+                }
+            }, 2000);
+        }
+
+        public void onLoadMore(final RefreshLayout refreshLayout) {
+            refreshLayout.getLayout().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mAdapter.loadMore(buildItems());
+                    if (mAdapter.getItemCount() > 60) {
+                        Toast.makeText(getContext(), "数据全部加载完毕", Toast.LENGTH_SHORT).show();
+                        refreshLayout.finishLoadMoreWithNoMoreData();//将不会再次触发加载更多事件
+                    } else {
+                        refreshLayout.finishLoadMore();
+                    }
+                }
+            }, 2000);
         }
     }
 }
