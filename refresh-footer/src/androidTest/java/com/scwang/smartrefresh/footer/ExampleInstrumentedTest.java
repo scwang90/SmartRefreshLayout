d5e6d8e8dee75726f1872dacf2f23620436a2d5f
wsc <scwang>
2024-11-07 15:28:04 +0800

    修复 xml 中错误的包名

--- refresh-footer/src/androidTest/java/com/scwang/smartrefresh/footer/ExampleInstrumentedTest.java
+++ refresh-footer/src/androidTest/java/com/scwang/smartrefresh/footer/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.scwang.smartrefresh.footer.test", appContext.getPackageName());
+        assertEquals("com.scwang.smart.refresh.footer.test", appContext.getPackageName());
     }
 }
--- refresh-layout-kernel/src/androidTest/java/com/scwang/smart/refresh/layout/ExampleInstrumentedTest.java
+++ refresh-layout-kernel/src/androidTest/java/com/scwang/smart/refresh/layout/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.scwang.smartrefresh.layout.test", appContext.getPackageName());
+        assertEquals("com.scwang.smart.refresh.layout.test", appContext.getPackageName());
     }
 }
